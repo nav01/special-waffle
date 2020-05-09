5096ea22daf74ed30d4a628b6ae136b4e9c1b514
Navjeet Singh <nav01@mail.fresnostate.edu>
2020-05-08 17:46:41 -0700

    Added google sign in activity.  Implemented basic sign in functionality to display email on main activity.

--- app/src/main/java/com/example/kusdinventoryscanner/MainActivity.java
+++ app/src/main/java/com/example/kusdinventoryscanner/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.kusdinventoryscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOGIN_REQUEST = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (true) { //logged in?
+            Intent loginIntent = new Intent(this, SignIn.class);
+            startActivityForResult(loginIntent, LOGIN_REQUEST);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LOGIN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                TextView textView = (TextView) findViewById(R.id.placeholderText);
+                String loginString = data.getExtras().getString(SignIn.LOGIN_EXTRA);
+                textView.setText(loginString);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/kusdinventoryscanner/SignIn.java
@@ -0,0 +1,73 @@
+package com.example.kusdinventoryscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SignIn extends AppCompatActivity {
+
+    public static final String LOGIN_EXTRA = "login_extra";
+
+    private static final int GOOGLE_SIGN_IN_REQUEST = 0;
+    private static final String TAG = SignIn.class.getSimpleName();
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSignInClick();
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    public void onSignInClick() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN_REQUEST) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            getIntent().putExtra(LOGIN_EXTRA, account.getEmail());
+            setResult(Activity.RESULT_OK, getIntent());
+            finish();
+            //updateUI(account);
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code=" + e);
+            //updateUI(null);
+        }
+    }
+}
